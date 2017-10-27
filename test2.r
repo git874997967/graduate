9f2e96f5ea471cf0e3c7bdac26116c0337e20506
git874997967 <g874997967@gmail.com>
2017-10-26 22:31:19 -0700

    基本笔记模型已经整理完毕 周末 确定最终版代码  并且开始ppt 制作

--- test2.r
+++ test2.r
@@ -1,7 +1,10 @@
 library(GGally)
 library(rpart)
 library(doParallel)
+library(randomForest)
 library(parallel)
+library(mlbench)
+library(kknn)
 ###the file is test for some new models and skills about the graduate project.
 setwd('../Desktop/graduate')
 diamondsBig=read.csv('diamonds.csv')
@@ -13,21 +16,26 @@ diamond=na.omit(diamondsBig[c(-1,-9)])
 # filter(diamond,diamond$x!='0')
 # filter(diamond,diamond$z!='0')
 #####whether unique or not ???? 
- 
-  ggpairs(diamond[c(1,2,3),])
+ # cl=detectCores()
+ #   registerDoParallel(cl) 
+    
   ggpairs(diamond, title=' basic info about the diamond dataset',
           lower = list(continuous = wrap("points", shape = I('.'))), 
           upper = list(combo = wrap("box", outlier.shape = I('.'))))
-  
-pairs(diamond)
+   
+ 
+  # stopCluster(cl)
+ 
 ## parallel is not useful with the ggpairs
-system.time({
-  #cl=makeCluster(detectCores())
-#registerDoParallel(cl)
-#ggpairs(diamonds,columns = 1:9,title='basic info about diamond dataset')
- pairs(diamonds)
-})
-stopCluster(cl)
+ 
+ 
+ #  cl <- makeCluster(8, type = "SOCK")
+ #  registerDoSNOW(cl)
+ # #ggpairs(diamond,columns = 1:9,title='basic info about diamond dataset')
+ #  rf5=randomForest(price~.,data=train,ntree=30)
+ #pairs(diamonds)
+ 
+# stopCluster(cl)
 pairPlot
 sampleData=sample(2,nrow(diamond),replace=TRUE,prob=c(0.85,0.15))
 test=diamond[sampleData==2,]
@@ -41,19 +49,8 @@ summary(step(lm3))
 glm1=glm(log10(price) ~I(carat^(1/3))+carat+cut+clarity+color+cert+table+depth+x+y+z+x*y*z, data = train,family=gaussian(link = "identity"))
 glm2=glm(log10(price) ~I(carat^(1/3))+carat+cut+clarity+color+cert+table+depth+x+y+z+x*y*z, data = train,family=quasipoisson(link = "log"))
 glm3=glm(log10(price) ~I(carat^(1/3))+carat+cut+clarity+color+cert+table+depth+x+y+z+x*y*z, data = train,family=quasi(link = "identity", variance = "constant"))
-         #### regresssion tree)
-summary
-
-tm4=rpart(price~., data=train,method="anova",control='cp=0.001',xval=10)
-printcp(tm4) # display the results 
-plotcp(tm4) # visualize cross-validation results 
-summary(tm4) # detailed summary of splits
-
-# plot tree 
-plot(tm4, uniform=TRUE, 
-     main="Regression tree for price")
-text(tm4, use.n=TRUE, all=TRUE, cex=.8)
-######prune is to provent overfitting
+        
+  #### regresssion tree)
 ####benefits
 # 决策树的够造不需要任何领域知识，就是简单的IF...THEN...思想 ；
 # 
@@ -64,6 +61,30 @@ text(tm4, use.n=TRUE, all=TRUE, cex=.8)
 # 4）决策树在运算过程中也是非常迅速的；
 # 
 # 5）一般而言，决策树还具有比较理想的预测准确率。
+tm4=rpart(price~., data=train,method="anova",control='cp=0.001',xval=10)
+printcp(tm4) # display the results 
+plotcp(tm4) # visualize cross-validation results 
+summary(tm4) # detailed summary of splits
+
+# plot tree 
+plot(tm4, uniform=TRUE, 
+     main="Regression tree for price")
+text(tm4, use.n=TRUE, all=TRUE, cex=.8)
+######prune is to provent overfitting
+#cp: complexity parameter, 复杂性参数，
+#用来设定修剪用的参数，一般遵循1-SE规则。
+#http://f.dataguru.cn/thread-315284-1-1.html
+### 使用1-SE法则选出最优cp值:找到xerror最小的行，得到误差阈值为该行的xerror+xstd
+## 找到所有xerror小于这个阈值的行，取其中最大值的上限为prune的阈值
+# in this model we use post -pruning
+# CP: complexity parameter
+# nsplit: the number of splits
+# rel error: the relative error rate for
+# predictions
+# for the data that generated the tree
+# xerror: cross-validated error.x in xerror is
+# an abbreviation for cross-validated
+# xstd: the standard derivation of crossvalidated
 ptm4=prune(tm4,cp=0.011574)
 plot(ptm4, uniform=TRUE, 
      main="Pruned Regression Tree for price1")
@@ -83,7 +104,7 @@ summary(fit) # detailed summary of splits
 
 # create additional plots 
 par(mfrow=c(1,2)) # two plots on one page 
-rsq.rpart(fit) # visualize cross-validation results  	
+rsq.rpart(pfit) # visualize cross-validation results    
 
 # plot tree 
 plot(fit, uniform=TRUE, 
@@ -91,12 +112,29 @@ plot(fit, uniform=TRUE,
 text(fit, use.n=TRUE, all=TRUE, cex=.8)
 #### prune tree
 # prune the tree 
-pfit<- prune(fit, cp=0.01160389) # from cptable   
+pfit<- prune(fit, cp=0.010000) # from cptable   
 
 # plot the pruned tree 
 plot(pfit, uniform=TRUE, 
      main="Pruned Regression Tree for Mileage")
 text(pfit, use.n=TRUE, all=TRUE, cex=.8)
-#randomForest   no enough memory
-rf5=randomForest(price~.,data=train)
+
+
+# kknn   knn one of the easist algo  in the ML
+# one example of kknn
+# https://www.rdocumentation.org/packages/kknn/versions/1.3.1/topics/kknn
+data(ionosphere)
+ionosphere.learn <- diamonds[1:200,1:10]
+ionosphere.valid <- diamonds[-c(1:200),1:10]
+ 
+fit.kknn <- kknn(price ~ ., ionosphere.learn, ionosphere.valid)
+table(ionosphere.valid$class, fit.kknn$fit)
+(fit.train1 <- train.kknn(price ~ ., ionosphere.learn, kmax = 15, 
+                          kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 1))
+table(predict(fit.train1, ionosphere.valid), ionosphere.valid$price)
+(fit.train2 <- train.kknn(price ~ ., ionosphere.learn, kmax = 15, 
+                          kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 2))
+table(predict(fit.train2, ionosphere.valid), ionosphere.valid$price)
   
+
+
