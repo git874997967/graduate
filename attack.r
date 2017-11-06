d2401f166dbe9eede2d92313acf2d578d9b881a7
git874997967 <g874997967@gmail.com>
2017-11-06 01:00:32 -0800

    refine attack file

--- attack.r
+++ attack.r
@@ -9,22 +9,22 @@ test =function(x){
   # generate the url 
   url=paste('http://guansmushroom.com',sep='')
   # load the webpage
-  webpage=read_html(url)
+
   print('load page success')
   node=html_nodes(webpage,'td')
   nodedata=html_text(node)
   # get each element
-  for (i in 0:20000)
-  {
-     
-    
-  }
+  
   # save the data frame to a file
   write.csv(total,'diamonds.csv')
 }
 cl=makeCluster(detectCores(logical = TRUE))
 registerDoParallel(cl)
-foreach(x=0:299000,combin='rbind' %dopar% test(x))
+ for (i in 0:20000)
+  {
+       webpage=read_html(url)
+      print(i)
+  }
 stopCluster()
 cl 
 
